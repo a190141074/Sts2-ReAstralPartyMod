f5dcdcd656c96b55784c791979187494d5e41663
loje1114 <a190141074@gmail.com>
2026-04-19 02:18:15 +0800

    稳定版-2026-04-19-2

--- AstralPartyCardCode/Relics/PersonWeirdEgg.cs
+++ AstralPartyCardCode/Relics/PersonWeirdEgg.cs
@@ -1,4 +1,5 @@
 using System;
+using System.Collections.Generic;
 using System.Threading.Tasks;
 using AstralPartyMod.AstralPartyCardCode.cards;
 using BaseLib.Utils;
--- AstralPartyCardCode/Utils/RecursiveCallGuard.cs
+++ AstralPartyCardCode/Utils/RecursiveCallGuard.cs
@@ -1,3 +1,6 @@
+using System;
+using System.Threading.Tasks;
+
 namespace AstralPartyMod.AstralPartyCardCode.Utils;
 
 public class RecursiveCallGuard<T>(Func<T, Task> action, Func<T, bool>? shouldProcess = null)
